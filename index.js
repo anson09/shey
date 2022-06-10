67567ba238aadc346d667e247694c09606c9dc05
anson <anson@ricequant.com>
2022-06-10 16:00:36 +0800

    feat: support merge history

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 #!/usr/bin/env node
 
 import { readFile } from "node:fs/promises";
+import { merge } from "./lib/merge.js";
 import { countFrequency } from "./lib/frequency.js";
 
 const rawFile = await readFile("zsh_history.local", { encoding: "utf8" });
+const rawFile2 = await readFile("zsh_history.remote", { encoding: "utf8" });
 
-countFrequency(rawFile);
+merge(rawFile, rawFile2);
+// countFrequency(rawFile);
