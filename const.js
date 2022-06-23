fce24f98dc4213cd9cffa296b7992cd515eb2861
anson <anson@ricequant.com>
2022-06-23 16:54:28 +0800

    feat: support default zsh file path

--- const.js
+++ const.js
@@ -1,1 +1,4 @@
+import { homedir } from "node:os";
+
 export const DEFAULT_EXPORT_PATH = "zsh_history.shey";
+export const ZSH_HISTORY_PATH = homedir() + "/.zsh_history";
--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 import { program } from "commander";
 import { createRequire } from "module";
+import { ZSH_HISTORY_PATH } from "./const.js";
 import merge from "./lib/merge.js";
 import frequency from "./lib/frequency.js";
 import pure from "./lib/pure.js";
@@ -14,13 +15,13 @@ program.name(name).description(description).version(version);
 program
   .command("pure")
   .description("cleanup format uncorrect line and sort by time")
-  .argument("<shell_history>", "shell history file path")
+  .argument("[shell_history]", "shell history file path", ZSH_HISTORY_PATH)
   .action(pure);
 
 program
   .command("frequency")
   .description("count usage frequency of command")
-  .argument("<shell_history>", "shell history file path")
+  .argument("[shell_history]", "shell history file path", ZSH_HISTORY_PATH)
   .action(frequency);
 
 program
