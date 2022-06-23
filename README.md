2f8162479d50d91331c685d7c9a3b2e8268fdf5d
anson <anson@ricequant.com>
2022-06-23 11:30:39 +0800

    feat: add cli support

--- README.md
+++ README.md
@@ -1,5 +1,16 @@
-- cli app
-- currently zsh support
-- welcome pr enhance robust
-- merge history files by time
-- list sort by frequence
+# Shey
+
+Shey is a simple and easy-to-use CLI tool for managing your shell history. It offers these functions:
+
+![screen shot of Shey](./assets/ScreenShot.png)
+
+Currently supports ZSH history only, welcome PR to add more formats support
+
+## Usage
+
+1. npm -g install shey
+2. shey
+
+## TODO
+
+- [ ] add release
--- index.js
+++ index.js
@@ -1,9 +1,33 @@
 #!/usr/bin/env node
 
+import { program } from "commander";
+import { createRequire } from "module";
 import merge from "./lib/merge.js";
 import frequency from "./lib/frequency.js";
 import pure from "./lib/pure.js";
 
-await merge("zsh_history.local", "zsh_history.remote");
-await pure("zsh_history.shey");
-await frequency("zsh_history.shey");
+const require = createRequire(import.meta.url);
+const { name, description, version } = require("./package.json");
+
+program.name(name).description(description).version(version);
+
+program
+  .command("pure")
+  .description("cleanup format uncorrect line and sort by time")
+  .argument("<shell_history>", "shell history file path")
+  .action(pure);
+
+program
+  .command("frequency")
+  .description("count usage frequency of command")
+  .argument("<shell_history>", "shell history file path")
+  .action(frequency);
+
+program
+  .command("merge")
+  .description("merge two shell history file")
+  .argument("<shell_history_1>", "first shell history file path")
+  .argument("<shell_history_2>", "second shell history file path")
+  .action(merge);
+
+program.parse();
